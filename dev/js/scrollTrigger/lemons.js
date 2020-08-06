a0d53635e701983936b2c042bcc497c8e1b22dd6
peanvtman <63323238+peanvtman@users.noreply.github.com>
2020-08-06 17:15:24 -0400

    lemons on hold
    josh doesn't know whats wrong with them so ill get back to this

--- dev/js/scrollTrigger/lemons.js
+++ dev/js/scrollTrigger/lemons.js
@@ -1,14 +1,15 @@
 var lemonsTimeline = gsap.timeline();
-lemonsTimeline.to(".lemon", {duration:5, rotate: 360, repeat:-1, repeatDelay: 0})
+lemonsTimeline.to(".lemon", {duration:10, rotate: 360, repeat:5, repeatDelay: 1})
 
 ScrollTrigger.create({
     trigger: "#lemons",
     start: "top 60%",
     end: "bottom 30%",
     animation: lemonsTimeline,
-    toggleActions: "restart play reverse none",
+    toggleActions: "restart play play stop",
     // scrub: 1
-    markers: true
+    // markers: true,
+    id:"lemon"
 });
 
 gsap.set(".lemon",{transformOrigin:"center center"});
