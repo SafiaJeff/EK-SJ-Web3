2e4071081bb83f11466c2e93b8c10ef9113af83f
peanvtman <63323238+peanvtman@users.noreply.github.com>
2020-08-05 21:23:54 -0400

    Lemonade test
    - scroll down, liuid goes out of glass

--- dev/js/scrollTrigger/down-arrow.js
+++ dev/js/scrollTrigger/down-arrow.js
@@ -13,3 +13,17 @@ ScrollTrigger.create({
     scrub: 1
     // markers: true
 });
+
+
+var liquidTimeline = gsap.timeline();
+liquidTimeline.to("#liquid",{duration:5, yPercent: 20})
+
+ScrollTrigger.create({
+    trigger: "#child",
+    start: "top 30%",
+    end: "bottom 40%",
+    animation: liquidTimeline,
+    toggleActions: "restart none reverse none",
+    scrub: 1,
+    markers: true
+});
