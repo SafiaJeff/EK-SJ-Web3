d88eb183674ff3c31e9953c73c9f250111bd3717
peanvtman <63323238+peanvtman@users.noreply.github.com>
2020-08-08 18:57:57 -0400

    cleaned up JS files
    I deleted all the unused JS files

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -5,12 +5,7 @@
 
 // @codekit-prepend "scrollTrigger/lemons.js";
 // @codekit-prepend "scrollTrigger/sun.js";
-
-// @codekit-prepend "scrollTrigger/blush.js";
 // @codekit-prepend "scrollTrigger/juice.js";
-// @codekit-prepend "scrollTrigger/dimples.js";
-// @codekit-prepend "scrollTrigger/mouth.js";
-// @codekit-prepend "scrollTrigger/eyebrows.js";
 // @codekit-prepend "scrollTrigger/combinedTimeline.js";
 // @codekit-prepend "scrollTrigger/childPin.js";
 
