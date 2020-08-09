73070138f7f51fb5e44278387200de576e431df9
peanvtman <63323238+peanvtman@users.noreply.github.com>
2020-08-09 17:07:55 -0400

    added third lemon

--- dev/js/scrollTrigger/lemons.js
+++ dev/js/scrollTrigger/lemons.js
@@ -1,6 +1,7 @@
 var lemonsTimeline = gsap.timeline();
 lemonsTimeline.to("#lemon1", {duration:10, rotation:360, repeat:-1, ease: "none"},"lemon")
                 .to("#lemon2", {duration:15, rotation:360, repeat:-1, ease: "none"},"lemon")
+                .to("#lemon3", {duration:8, rotation:-360, repeat:-1, ease: "none"},"lemon")
 
 ScrollTrigger.create({
     trigger: "#lemons",
@@ -13,7 +14,7 @@ ScrollTrigger.create({
     id:"lemon"
 });
 
-gsap.set("#lemon1, #lemon2",{transformOrigin:"center center"});
+gsap.set("#lemon1, #lemon2, #lemon3",{transformOrigin:"center center"});
 
 // gsap.to("#lemon",{duration:1, rotation:360, repeat:-1, ease: "none"})
 
