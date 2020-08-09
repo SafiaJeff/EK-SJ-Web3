7139c0ef65df4a3c21ca88a449e9b2234721cb61
peanvtman <63323238+peanvtman@users.noreply.github.com>
2020-08-08 21:13:08 -0400

    bg color change cont

--- dev/js/scrollTrigger/bgColorChange.js
+++ dev/js/scrollTrigger/bgColorChange.js
@@ -1,15 +1,16 @@
 // gsap.to("#content",{duration:10, backgroundColor:"#B1DFFC"});
 
 var bgTimeline = gsap.timeline();
-bgTimeline.to("#content",{backgroundColor:"#B1DFFC"});
+bgTimeline.to("#content",{backgroundColor:"#B1DFFC"})
+            // .to("#content",{backgroundColor:"#FFFDF3"});
                 
 
 ScrollTrigger.create({
-    trigger: "#sun",
+    trigger: "#color-change-bg",
     start: "top 60%",
-    end: "bottom 30%",
+    end: "bottom 50%",
     animation: bgTimeline,
-    toggleActions: "restart play play stop",
+    toggleActions: "restart play none none",
     scrub: 1,
     markers: true,
     id:"bg-color-change"
