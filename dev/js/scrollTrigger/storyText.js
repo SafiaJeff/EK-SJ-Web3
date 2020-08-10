c7b9d417df630097774699970691312744cb58f8
peanvtman <63323238+peanvtman@users.noreply.github.com>
2020-08-10 09:53:35 -0400

    added variable to text
    - added variable to text xpercents
    - removed border from bottom of child

--- dev/js/scrollTrigger/storyText.js
+++ dev/js/scrollTrigger/storyText.js
@@ -1,11 +1,20 @@
+var leftXPercent =10;
+var RightXPercent = -10;
+
 var storyTextTimeline = gsap.timeline();
-storyTextTimeline.from("#part1L", {alpha:0, xPercent:10})
-                .from("#part1R", {alpha:0, xPercent:-10})
-                .from("#part2L", {alpha:0, xPercent:10})
-                .from("#part2R", {alpha:0, xPercent:-10})
-                .from("#part3L", {alpha:0, xPercent:10})
-                .from("#part3R", {alpha:0, xPercent:-10})
-                .from("#part4L", {alpha:0, xPercent:10})
+storyTextTimeline.from("#part1L", {alpha:0, xPercent:leftXPercent})
+                .from("#part1R", {alpha:0, xPercent:RightXPercent})
+                .from("#part2L", {alpha:0, xPercent:leftXPercent})
+                .from("#part2R", {alpha:0, xPercent:RightXPercent})
+                .from("#part3L", {alpha:0, xPercent:leftXPercent})
+                .from("#part3R", {alpha:0, xPercent:RightXPercent})
+                .from("#part4L", {alpha:0, xPercent:leftXPercent})
+
+// storyTextTimeline
+// .from("#part1R", {alpha:0, xPercent:-10})
+// .from("#part2R", {alpha:0, xPercent:-10})
+// .from("#part3R", {alpha:0, xPercent:-10})
+
 
 
 ScrollTrigger.create({
